8be7fb59692aa68be0f7268dc3642000789e42de
lucarin91 <lucarin91@gmail.com>
2017-06-12 13:49:28 +0200

    fix docker-compose and add new parameter on the start scripts

--- replaceability/backend2/scripts/start.sh
+++ replaceability/backend2/scripts/start.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 abs_path=$(cd $(dirname $0)/.. && pwd)
 
-if [ ! -f $abs_path/pid ];then
-  cd $abs_path
-  node app/bin/www > /dev/null &
-  echo $! > $abs_path/pid
-  echo 'started!'
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --wait)
+      wait=y
+      shift
+      break
+      ;;
+    *)
+      echo 'error parameter'
+      exit 1
+      ;;
+  esac
+done
+
+cd $abs_path
+if [ "$wait" != "y" ]; then
+  if [ ! -f $abs_path/pid ];then
+    node app/bin/www > /dev/null &
+    echo $! > $abs_path/pid
+    echo 'started!'
+  fi
+else
+  node app/bin/www
 fi
