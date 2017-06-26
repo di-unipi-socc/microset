5a2e72c0c0e7cdeafd27861f40e89c3c075005f2
lucarin91 <lucarin91@gmail.com>
2017-06-26 11:31:28 +0200

    fix issue #6

--- examples/docker-compose/horizontal-scalability/script.sh
+++ examples/docker-compose/horizontal-scalability/script.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -xe
 
 docker-compose -f docker-compose.yml up -d
-docker-compose -f docker-compose.yml scale consumer=2
+
+sleep 1
+docker-compose -f docker-compose.yml scale consumer=3
 
 sleep 20
-curl -f http://127.0.0.1:3000/
+curl -f http://127.0.0.1:3000/api
